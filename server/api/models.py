4035fdc6486a81b597f53a545a76d87949167e99
firatkizilboga <firatkizilboga11@gmail.com>
2023-03-12 00:04:24 +0300

    Added the rest of the models and a new one ItemSubscription

--- server/api/models.py
+++ server/api/models.py
@@ -37,8 +37,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class Order(models.Model):
-    pass
 
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -50,6 +48,92 @@ class Address(models.Model):
     postal_code = models.CharField(max_length=255, blank=True, null=True)
     country = models.CharField(max_length=255, blank=True, null=True)
 
+class Item(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
+from datetime import timedelta
 class Subscription(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullfillment_frequency = models.IntegerField(default=30)
+    start_date = models.DateField(auto_now_add=True)
+
+    last_fullfillment_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+    @property
+    def items(self):
+        return ItemSubscription.objects.filter(subscription=self)
+    
+    @property
+    def total(self):
+        total = 0
+        for item in self.items:
+            total += item.item.price * item.quantity
+        return total
+    
+    @property
+    def next_fullfillment_date(self):
+        return self.last_fullfillment_date + timedelta(days=self.fullfillment_frequency)
+    
+
+    #these functions are for the instance of the subscription
+    def fullfill(self):
+        self.last_fullfillment_date = self.next_fullfillment_date
+        self.save()
+
+
+    
+    def cancel(self):
+        self.is_active = False
+        self.save()
+    
+    def reactivate(self):
+        self.is_active = True
+        self.save()
+    
+#create a Item Subscription relation
+class ItemSubscription(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+from datetime import datetime
+class Order(models.Model):
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+    is_paid_date = models.DateTimeField(blank=True, null=True)
+    is_shipped = models.BooleanField(default=False)
+    is_shipped_date = models.DateTimeField(blank=True, null=True)
+    is_delivered = models.BooleanField(default=False)
+    is_delivered_date = models.DateTimeField(blank=True, null=True)
+    is_cancelled = models.BooleanField(default=False)
+    is_cancelled_date = models.DateTimeField(blank=True, null=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.total_price or self.total_price == 0:
+            self.total_price = self.subscription.total
+        super(Order, self).save(*args, **kwargs)
+
+    def cancel(self):
+        self.is_cancelled = True
+        self.is_cancelled_date = datetime.now()
+        self.save()
+    
+    def pay(self):
+        self.is_paid = True
+        self.is_paid_date = datetime.now()
+        self.save()
+    
+    def ship(self):
+        self.is_shipped = True
+        self.is_shipped_date = datetime.now()
+        self.save()
+    
+    def deliver(self):
+        self.is_delivered = True
+        self.is_delivered_date = datetime.now()
+        self.save()
